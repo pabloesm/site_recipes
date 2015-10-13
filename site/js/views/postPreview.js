d0e2467453632489921075768fa54c34babb9bbc
pabloesm <pabloe85@gmail.com>
2015-10-13 18:26:55 +0200

    post preview

--- site/js/views/postPreview.js
+++ site/js/views/postPreview.js
@@ -20,9 +20,17 @@ module.exports = Backbone.View.extend({
 	},
 
 	render: function() {
+		var previewAttrs = $.extend({}, this.model.attributes),
+			plainBody = $(previewAttrs.body).text(),
+			TRIM_LENGTH = 117,
+			trimmedBody = this.trimString(plainBody, TRIM_LENGTH);
+
+		previewAttrs.body = '<p>' + trimmedBody + '</p>';
+
 		var source =$('#postPreviewTemplate').html();
-		var template = Handlebars.compile(source);
-		this.$el.html(template(this.model.attributes));
+		var	template = Handlebars.compile(source);
+
+		this.$el.html(template(previewAttrs));
 
 		return this;
 	},
@@ -31,6 +39,24 @@ module.exports = Backbone.View.extend({
 		var idReadable = this.model.get('idReadable');
 		var url = "#post/" + idReadable;
 		window.location.href = url;
+	},
+
+	trimString: function(s, length) {
+		/* Trims a given string to the maximum length without truncate any word*/
+		var words = s.split(' ');
+		var trimmedStr = words[0]
+		for (var i = 1; i < words.length; i++) {
+			trimmedStr += ' ' + words[i]
+
+			if (words[i+1]) {
+				var n = trimmedStr + ' ' + words[i+1]
+				if (n.length > (length - 3)) {
+					return trimmedStr + '...';
+				}
+			}
+
+		}
+		return trimmedStr;
 	}
 
 });
